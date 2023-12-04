9fbf4f2e8d40bb2d51d66c7c3de2e67f7dee8343
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-12-03 21:39:43 -0300

    projeto criado

---
+++ at_test/Data/Models/ProdutoModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace at_test.Data.Models
+{
+    public class ProdutoModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "O nome deve ter pelo menos 2 caracteres")]
+        public string Nome { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "A descrição deve ter pelo menos 5 caracteres")]
+        public string Descricao { get; set; }
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "O preço deve ser um valor positivo")]
+        public float Preco { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "A quantidade deve ser um valor positivo")]
+        public int Quantidade { get; set; }
+        public string NomeImagem { get; set; }
+        [NotMapped]
+        public IFormFile Upload { get; set; }
+    }
+}
