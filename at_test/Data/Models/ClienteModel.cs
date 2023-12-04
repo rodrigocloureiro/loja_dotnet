0eb6cc460f933aaeecc4e0ae793ae0541eb5ed08
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-12-04 19:07:55 -0300

    classe cliente e tabela clientes implementadas

---
+++ at_test/Data/Models/ClienteModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace at_test.Data.Models
+{
+    public class ClienteModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [MinLength(11, ErrorMessage = "O CPF deve ter pelo menos 11 caracteres"), MaxLength(14, ErrorMessage = "O CPF deve ter no máximo 14 caracteres")]
+        public string Cpf { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        [Required]
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ at_test/Migrations/20231204215937_TableClientes.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace at_test.Migrations
+{
+    public partial class TableClientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(14)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
