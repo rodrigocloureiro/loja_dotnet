db1f50abc10d5878a1c969d3e07548cc566ee9b2
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-12-03 21:47:07 -0300

    context criado

---
+++ at_test/Data/EsportivaContext.cs
@@ -0,0 +1,12 @@
+﻿using at_test.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace at_test.Data
+{
+    public class EsportivaContext : DbContext
+    {
+        public EsportivaContext(DbContextOptions<EsportivaContext> options) : base(options) { }
+
+        public DbSet<ProdutoModel> Produtos { get; set; }
+    }
+}
