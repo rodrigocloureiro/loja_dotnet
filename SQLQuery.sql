88c7468dbb7e4b273c332750d890d351c9424f91
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-12-05 00:08:01 -0300

    querySQL ordenando clintes do mais atual e total de registros

---
+++ SQLQuery.sql
@@ -0,0 +1,26 @@
+﻿INSERT INTO
+	Produtos
+VALUES
+	('Esse produto será deletado', 'Esse produto tem uma função: SER DELETADO', '999.99', '999', NULL);
+
+SELECT
+	*
+FROM
+	Produtos;
+
+SELECT
+	*
+FROM
+	Clientes;
+
+SELECT
+	*
+FROM
+	Clientes
+ORDER BY
+	Clientes.DataCadastro
+DESC;
+
+SELECT COUNT(*) FROM Clientes;
+SELECT COUNT(Id) FROM Clientes;
+SELECT COUNT(Nome) FROM Clientes;
