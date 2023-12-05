03ede397373c38c48910bd063cba4143ef087d3c
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-12-05 20:59:06 -0300

    adicionando propriedade DataRegistro ao Produto e validacao do lado so cliente implementada

--- at_test/Data/Models/ClienteModel.cs
+++ at_test/Data/Models/ClienteModel.cs
@@ -6,12 +6,12 @@ namespace at_test.Data.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira o seu nome")]
         public string Nome { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira o seu CPF")]
         [MinLength(11, ErrorMessage = "O CPF deve ter pelo menos 11 caracteres"), MaxLength(14, ErrorMessage = "O CPF deve ter no máximo 14 caracteres")]
         public string Cpf { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira a sua data de nascimento")]
         public DateTime DataNascimento { get; set; }
         [Required]
         public DateTime DataCadastro { get; set; }
--- at_test/Data/Models/ProdutoModel.cs
+++ at_test/Data/Models/ProdutoModel.cs
@@ -7,16 +7,16 @@ namespace at_test.Data.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira o nome do produto")]
         [MinLength(2, ErrorMessage = "O nome deve ter pelo menos 2 caracteres")]
         public string Nome { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira uma descrição para o produto")]
         [MinLength(5, ErrorMessage = "A descrição deve ter pelo menos 5 caracteres")]
         public string Descricao { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira um valor para o preço")]
         [Range(0, int.MaxValue, ErrorMessage = "O preço deve ser um valor positivo")]
         public decimal Preco { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Por favor, insira um valor para a quantidade")]
         [Range(0, int.MaxValue, ErrorMessage = "A quantidade deve ser um valor positivo")]
         public int Quantidade { get; set; }
         [Required]
