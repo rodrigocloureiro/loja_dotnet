a76d0501b85f822815a022a802f55dac17670481
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-12-06 14:50:06 -0300

    refatoracao na validacao do CPF

--- at_test/Data/Models/ClienteModel.cs
+++ at_test/Data/Models/ClienteModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.ConstrainedExecution;
 
 namespace at_test.Data.Models
 {
@@ -9,7 +10,10 @@ namespace at_test.Data.Models
         [Required(ErrorMessage = "Por favor, insira o seu nome")]
         public string Nome { get; set; }
         [Required(ErrorMessage = "Por favor, insira o seu CPF")]
-        [MinLength(11, ErrorMessage = "O CPF deve ter pelo menos 11 caracteres"), MaxLength(14, ErrorMessage = "O CPF deve ter no máximo 14 caracteres")]
+        //[MinLength(11, ErrorMessage = "O CPF deve ter pelo menos 11 caracteres"), MaxLength(14, ErrorMessage = "O CPF deve ter no máximo 14 caracteres")]
+        [MaxLength(11, ErrorMessage = "O CPF deve ter no máximo 11 caracteres")]
+        //[RegularExpression(@"^\d{3}\.\d{3}\.\d{3}\-\d{2}$", ErrorMessage = "O CPF deve seguir o formato 000.000.000-00")]
+        [RegularExpression(@"^[0-9]{11}$", ErrorMessage = "Por favor, informe o CPF com os 11 dígitos e sem pontuação")]
         public string Cpf { get; set; }
         [Required(ErrorMessage = "Por favor, insira a sua data de nascimento")]
         public DateTime DataNascimento { get; set; }
---
+++ at_test/Migrations/20231206174355_ModifyingCPFSize.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace at_test.Migrations
+{
+    public partial class ModifyingCPFSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Clientes",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Clientes",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+        }
+    }
+}
